3620d12e231864c8825b891f06bd0e4c69aa55cb
unknown <edokasomtochukwu@gmail.com>
2025-09-07 11:13:26 +0100

    completed projects

--- libs/action.js
+++ libs/action.js
@@ -1,6 +1,9 @@
 "use server";
 import { save } from "./meals";
 import { redirect } from "next/navigation";
+function InvalidText(text) {
+  return !text || text.trim() ===""
+}
  export async function shareMeal(formData) {
 
 const meal = {
@@ -11,6 +14,20 @@ const meal = {
   creator: formData.get("name"),
   creator_email : formData.get("email")
  
+}
+if (
+  InvalidText(meal.title || 
+  InvalidText(meal.summary || InvalidText(meal.creator) ) 
+  ||InvalidText( meal.creator_email) ||
+   InvalidText(meal.image)) 
+   ||InvalidText(meal.instructions)
+   ||
+   !meal.creator_email.includes("@")
+||
+   !meal.image || meal.image.size ===0
+  ) {
+throw new Error("invalid Input")
+  
 }
 
 await save(meal)
